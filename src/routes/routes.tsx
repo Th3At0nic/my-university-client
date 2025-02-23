e731b4a852abda73a01bc38e4ed30648fff1ed84
Th3At0nic <islammdrahatul@gmai.com>
2025-02-23 18:03:29 +0600

    done: dashboard,create-stu route, outlet

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,8 @@ import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import AdminDashboard from "../pages/admin/AdminDashboard";
+import CreateStudent from "../pages/admin/CreateStudent";
 
 export const router = createBrowserRouter([
   {
@@ -20,7 +22,24 @@ export const router = createBrowserRouter([
       },
     ],
   },
-
+  {
+    path: "/admin",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AdminDashboard />,
+      },
+      {
+        path: "dashboard",
+        element: <AdminDashboard />,
+      },
+      {
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+    ],
+  },
   {
     path: "/login",
     element: <Login />,
