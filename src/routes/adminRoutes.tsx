97bc6633bfab3cbad370f769ff543d275613697e
Th3At0nic <islammdrahatul@gmail.com>
2025-02-26 21:29:14 +0600

    added dynamic sidebarItemsGenerator, types folder, routes(faculty,students), dashboard(faculty,student,admin), plain components

--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -1,20 +1,7 @@
-import { ReactNode } from "react";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateStudent from "../pages/admin/CreateStudent";
-import CreateFaculty from "../pages/faculty/CreateFaculty";
-import { NavLink } from "react-router-dom";
-
-type TRoute = {
-  path: string;
-  element: ReactNode;
-};
-
-type TSidebarItem = {
-  key: string;
-  label: ReactNode;
-  children?: TSidebarItem[];
-};
+import CreateFaculty from "../pages/admin/CreateFaculty";
 
 export const adminPaths = [
   {
@@ -58,47 +45,3 @@ export const adminPaths = [
     ],
   },
 ];
-
-export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-  if (item.path && item.element) {
-    acc.push({
-      path: item.path,
-      element: item.element,
-    });
-  }
-
-  if (item.children) {
-    item.children.forEach((child) => {
-      acc.push({
-        path: child.path,
-        element: child.element,
-      });
-    });
-  }
-
-  return acc;
-}, []);
-
-export const adminSidebarItems = adminPaths.reduce(
-  (acc: TSidebarItem[], item) => {
-    if (item.name && item.path) {
-      acc.push({
-        key: item.name,
-        label: <NavLink to={`/admin/${item.path}`}> {item.name} </NavLink>,
-      });
-    }
-
-    if (item.children) {
-      acc.push({
-        key: item.name,
-        label: item.name,
-        children: item.children.map((child) => ({
-          key: child.name,
-          label: <NavLink to={`/admin/${child.path}`}> {child.name} </NavLink>,
-        })),
-      });
-    }
-    return acc;
-  },
-  []
-);
