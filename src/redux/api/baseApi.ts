0feabc37a6e429b97439b32aacb77eb6c90d6797
Th3At0nic <islammdrahatul@gmai.com>
2025-03-04 14:19:20 +0600

    done: fetch refreshToken, retrived new access token while expired, and set user to the state to avoid logout and smooth ux, fixed some bugs and error regardin refresh token being dropped by the browser for cookies setting in the backend, 28.5

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,12 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { RootState } from "../store";
+import { RootState } from "./../store";
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query/react";
+import { setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -15,8 +22,38 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithRefreshToken: BaseQueryFn<
+  string | FetchArgs, // 'args' type (URL string or an object with method, headers, body, etc.)
+  unknown, // Return type (usually inferred)
+  FetchBaseQueryError // Error type
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error?.status === 401) {
+    // Request a new token
+    const refreshResult = await fetch(
+      "http://localhost:5000/api/v1/auth/refresh-token",
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    ).then((response) => response.json());
+
+    const user = (api.getState() as RootState).auth.user;
+
+    api.dispatch(
+      setUser({
+        user,
+        token: refreshResult.data.accessToken,
+      })
+    );
+  }
+
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
