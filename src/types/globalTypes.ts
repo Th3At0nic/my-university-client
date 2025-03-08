945428f569e59e489bd8644c27d70bdc343d65d4
Th3At0nic <islammdrahatul@gmail.com>
2025-03-08 14:13:55 +0600

    show academicSemesters successfully, added antd table and colleborate with react hook form, filtering successfully, handled errors, 29.12, remaining: academicFaculty,dpt and view them

--- src/types/globalTypes.ts
+++ src/types/globalTypes.ts
@@ -1,3 +1,5 @@
+import { BaseQueryApi } from "@reduxjs/toolkit/query";
+
 export type TError = {
   data: {
     errorSource: { path: string; message: string }[];
@@ -8,19 +10,41 @@ export type TError = {
   status: number;
 };
 
-export type TResponse = {
-  data: {
-    code: string;
-    createdAt: string;
-    endMonth: string;
-    name: string;
-    startMonth: string;
-    updatedAt: string;
-    year: string;
-    __v: number;
-    _id: string;
-  };
-  message: string;
-  success: boolean;
-  error?: TError;
+export type TMeta = {
+  limit: number;
+  page: number;
+  total: number;
+  totalPage: number;
 };
+
+export type TResponse<T> = {
+  data?: T;
+  error?: TError;
+  meta?: TMeta;
+  success: boolean;
+  message: string;
+};
+
+export type TResponseRedux<T> = TResponse<T> & BaseQueryApi;
+
+export type TQueryParam = {
+  name: string;
+  value: boolean | React.Key;
+};
+
+// export type TResponse = {
+//   data: {
+//     code: string;
+//     createdAt: string;
+//     endMonth: string;
+//     name: string;
+//     startMonth: string;
+//     updatedAt: string;
+//     year: string;
+//     __v: number;
+//     _id: string;
+//   };
+//   message: string;
+//   success: boolean;
+//   error?: TError;
+// };
