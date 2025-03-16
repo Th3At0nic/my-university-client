5aa323d0abb182dc897d3bb9435fedd4aec4c7a8
Th3At0nic <islammdrahatul@gmai.com>
2025-03-16 22:11:53 +0600

    myStudents component of faculty is done, showed students based on sem and course for update marks

--- src/pages/faculty/MyCourses.tsx
+++ src/pages/faculty/MyCourses.tsx
@@ -1,13 +1,45 @@
-import { useGetAllEnrolledCoursesQuery } from "../../redux/features/student/studentCourseManagement.api";
+import { Flex, Col, Button } from "antd";
+import PHForm from "../../components/form/PHForm";
+import PHSelect from "../../components/form/PHSelect";
+import { useGetAllEnrolledCoursesQuery } from "../../redux/features/faculty/facultyCourseManagement.api";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const MyCourses = () => {
-  const { data } = useGetAllEnrolledCoursesQuery(undefined);
+  const navigate = useNavigate();
+  const { data: allEnrolledCourses } = useGetAllEnrolledCoursesQuery(undefined);
+  console.log(allEnrolledCourses);
 
-  console.log(data);
+  const semesterOptions = allEnrolledCourses?.data?.map((item) => ({
+    label: `${item.academicSemester.name} ${item.academicSemester.year}`,
+    value: item.semesterRegistration._id,
+  }));
+
+  const courseOptions = allEnrolledCourses?.data?.map((item) => ({
+    label: item.course.title,
+    value: item.course._id,
+  }));
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log("formData: ", data);
+    navigate(`/faculty/courses/${data.semesterRegistration}/${data.course}`);
+  };
+
   return (
-    <div>
-      <h1>Thi sis my courses</h1>
-    </div>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelect
+            label="Semester Registration"
+            name="semesterRegistration"
+            options={semesterOptions}
+          />
+          <PHSelect label="Course" name="course" options={courseOptions} />
+
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
---
+++ src/redux/features/faculty/facultyCourseManagement.api.ts
@@ -0,0 +1,32 @@
+import { TEnrolledCourse, TQueryParam, TResponseRedux } from "../../../types";
+import { baseApi } from "../../api/baseApi";
+
+const facultyCourseManagementApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllEnrolledCourses: builder.query({
+      query: (args) => {
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+        return {
+          url: "/enrolled-courses",
+          method: "GET",
+          params: params,
+        };
+      },
+      providesTags: ["enrolledCourses"],
+      transformResponse: (response: TResponseRedux<TEnrolledCourse[]>) => {
+        return {
+          data: response?.data,
+          meta: response?.meta,
+        };
+      },
+    }),
+  }),
+});
+
+export const { useGetAllEnrolledCoursesQuery } = facultyCourseManagementApi;
